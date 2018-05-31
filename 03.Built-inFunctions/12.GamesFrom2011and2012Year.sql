a6a138bbe91c702f54f33cc43d1deb8e7f8171b5
Katia Cherdoneva <32202527+katiacherdoneva@users.noreply.github.com>
2018-06-01 00:14:31 +0300

    Update 12.GamesFrom2011and2012Year.sql

--- 03.Built-inFunctions/12.GamesFrom2011and2012Year.sql
+++ 03.Built-inFunctions/12.GamesFrom2011and2012Year.sql
@@ -1,4 +1,4 @@
-USE Diablo
+--USE Diablo
 SELECT TOP (50) [Name], FORMAT([Start], 'yyyy-MM-dd') AS [Start]
   FROM Games
   WHERE DATEPART(YEAR, [Start]) = '2011' OR DATEPART(YEAR, [Start]) = '2012'
